a8fa21af63b18da1655fbadb026b8e7d5e5d9d28
GabrielCaldeira-25 <gabriel.caldeira7001@gmail.com>
2025-03-31 13:14:26 -0300

    canva2

--- AULA1.0/ex03.js
+++ AULA1.0/ex03.js
@@ -1,19 +1,33 @@
-function cadastrar() {
-    let nome = document.getElementById("nome").value;
-    let login = document.getElementById("login").value;
-    let senha = document.getElementById("senha").value;
+function cadastrar(){
+     let nome = document.getElementById('nome').value
+     let login = document.getElementById('login').value
+     let senha = document.getElementById('senha').value
 
-    if (localStorage.getItem("cadastro")) {
-        alert("Já existe um cadastro! Limpe antes de cadastrar um novo.");
+     if (localStorage.getItem("cadastro")){
+        alert('Já existe um cadastro, apague antes')
         return;
-    }
+     }
 
-    if (nome === "" || login === "" || senha === "") {
-        alert("Preencha todos os campos!");
-        return;
-    }
-
-    let usuario = { nome, login, senha };
-    localStorage.setItem("cadastro", JSON.stringify(usuario));
-    alert("Cadastro realizado com sucesso!");
+    let dados ={nome,login,senha}
+    localStorage.setItem("cadastro" , dados)
+    alert('Cadastro realizado com sucesso')
 }
+
+function limpar(){
+    localStorage.removeItem("cadastro")
+    alert('Dados apagados')
+
+}
+function mostrarDados() {
+    let dadosSalvos = localStorage.getItem("cadastro");
+
+    if (!dadosSalvos) {
+        alert("Nenhum cadastro encontrado!");
+        return;
+    }
+
+    let usuario = JSON.parse(dadosSalvos);
+    document.getElementById("dados").textContent = `Nome: ${usuario.nome}, Login: ${usuario.login}, Senha: ${usuario.senha}`;
+}
+
+
